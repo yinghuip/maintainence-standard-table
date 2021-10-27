f06d08812facffcb6af2eb8ce2576e3452ac53b1
yinghuip <yinghui90@live.com>
2021-10-27 22:12:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-gfapvq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-gfapvq)
---
+++ src/app/button-toggle-forms-example.ts
@@ -0,0 +1,225 @@
+import { visitValue } from '@angular/compiler/src/util';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
+
+/**
+ * @title Button-toggles with forms
+ */
+@Component({
+  selector: 'button-toggle-forms-example',
+  templateUrl: 'button-toggle-forms-example.html',
+})
+export class ButtonToggleFormsExample {
+  EnterLimitBy: 'Vehicle' | 'Axle' = 'Axle';
+  OverAndUnderLimit: 'Same' | 'Different' = 'Different';
+
+  VehicleAxles = [
+    {
+      Axle: 1,
+      Pressure: '',
+      Limit: '',
+      OverWarning: '',
+      OverCritical: '',
+      UnderWarning: '',
+      UnderCritical: '',
+    },
+    {
+      Axle: 2,
+      Pressure: '',
+      Limit: '',
+      OverWarning: '',
+      OverCritical: '',
+      UnderWarning: '',
+      UnderCritical: '',
+    },
+    {
+      Axle: 3,
+      Pressure: '',
+      Limit: '',
+      OverWarning: '',
+      OverCritical: '',
+      UnderWarning: '',
+      UnderCritical: '',
+    },
+    {
+      Axle: 4,
+      Pressure: '',
+      Limit: '',
+      OverWarning: '',
+      OverCritical: '',
+      UnderWarning: '',
+      UnderCritical: '',
+    },
+  ];
+
+  OverAndUnderDifferentHeader = [
+    {
+      Label: 'Axle',
+      Icon: null,
+      display: false,
+    },
+    {
+      Label: 'Pressure',
+      Icon: null,
+      display: true,
+    },
+    {
+      Label: 'Limit',
+      Icon: null,
+      display: true,
+    },
+    {
+      Label: 'Under Warning',
+      Icon: 'fas fa-exclamation-triangle warning',
+      display: true,
+    },
+    {
+      Label: 'Under Critical',
+      Icon: 'fas fa-exclamation-circle Critical',
+      display: true,
+    },
+    {
+      Label: 'Over Warning',
+      Icon: 'fas fa-exclamation-triangle warning',
+      display: true,
+    },
+    {
+      Label: 'Over Critical',
+      Icon: 'fas fa-exclamation-circle Critical',
+      display: true,
+    },
+  ];
+
+  OverAndUnderSameHeader = [
+    {
+      Label: 'Axle',
+      Icon: null,
+      display: false,
+    },
+    {
+      Label: 'Pressure',
+      Icon: null,
+      display: true,
+    },
+    {
+      Label: 'Limit',
+      Icon: null,
+      display: true,
+    },
+    {
+      Label: 'Warning',
+      Icon: 'fas fa-exclamation-triangle warning',
+      display: true,
+    },
+    {
+      Label: 'Critical',
+      Icon: 'fas fa-exclamation-circle Critical',
+      display: true,
+    },
+  ];
+
+  
+  UnderOverSameTopHeader = [
+    {
+      Label: 'Over / Under Inflation',
+      Icon: 'fas fa-arrows-alt-v',
+      display: true,
+    },
+  ];
+
+  UnderOverDifferentTopHeader = [
+    {
+      Label: 'Under Inflation',
+      Icon: 'fas fa-arrow-down',
+      display: true,
+    },
+    {
+      Label: 'Over Inflation',
+      Icon: 'fas fa-arrow-up',
+      display: true,
+    }
+  ];
+
+  topHeader = this.UnderOverDifferentTopHeader;
+
+
+  currentTableHeader = this.OverAndUnderDifferentHeader;
+  get ObjectKey() {
+    return Object.keys(this.VehicleAxles[0]);
+  }
+
+  get rowspan() {
+    if (this.EnterLimitBy === 'Vehicle') {
+      return this.VehicleAxles.length;
+    }
+    return 1;
+  }
+
+  renderLimit(i: number) {
+    if (this.EnterLimitBy === 'Vehicle' && i > 0) {
+      return false;
+    }
+    return true;
+  }
+
+  copyLimitToOtherAxle(value) {
+    this.VehicleAxles = this.VehicleAxles.map((s) => {
+      s.Limit = value;
+      return s;
+    });
+  }
+
+  changeHeader(event: MatButtonToggleChange) {
+    if (event.value === 'Same') {
+      this.currentTableHeader = this.OverAndUnderSameHeader;
+      this.topHeader = this.UnderOverSameTopHeader;
+    }
+    if (event.value === 'Different') {
+      this.currentTableHeader = this.OverAndUnderDifferentHeader;
+      this.topHeader = this.UnderOverDifferentTopHeader;
+    }
+  }
+
+  copyOverWarningToOtherAxle(value) {
+    this.VehicleAxles = this.VehicleAxles.map((s) => {
+      s.OverWarning = value;
+      return s;
+    });
+  }
+  copyOverCriticalToOtherAxle(value) {
+    this.VehicleAxles = this.VehicleAxles.map((s) => {
+      s.OverCritical = value;
+      return s;
+    });
+  }
+
+  copyUnderCriticaltToOtherAxle(value) {
+    this.VehicleAxles = this.VehicleAxles.map((s) => {
+      s.UnderCritical = value;
+      if (this.OverAndUnderLimit === 'Same') {
+        s.OverCritical = value;
+      }
+      return s;
+    });
+  }
+  copyUnderWarningToOtherAxle(value) {
+    this.VehicleAxles = this.VehicleAxles.map((s) => {
+      s.UnderWarning = value;
+      if (this.OverAndUnderLimit === 'Same') {
+        s.OverWarning = value;
+      }
+      return s;
+    });
+  }
+}
+
+export interface Header {
+  Label: string;
+  Icon: string;
+  display: boolean;
+}
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
